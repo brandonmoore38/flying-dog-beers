84f7abb682fcd4b92e95f3056135031aa4728ca7
brandonmoore38 <53958951+brandonmoore38@users.noreply.github.com>
2019-08-12 20:53:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import dash_html_components as html
 import plotly.graph_objs as go
 
 ########### Set up the chart
-beers=['Chesapeake Stout', 'Snake Dog IPA', 'Imperial Porter', 'Double Dog IPA']
-ibu_values=[35, 60, 85, 75]
+skanky beers=['Coors', 'Miller Lite', 'Bud Light', 'Budwiser']
+ibu_values=[12, 15, 20, 8]
 abv_values=[5.4, 7.1, 9.2, 4.3]
-color1='lightblue'
-color2='darkgreen'
+color1='navyblue'
+color2='lightblue'
 
 bitterness = go.Bar(
     x=beers,
